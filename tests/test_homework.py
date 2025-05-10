e563f98a65da673335d3afb5144cb4e8e323bffb
Alejandro Orozco Ochoa <alejooo636@gmail.com>
2025-05-10 10:39:50 -0500

    si

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -21,8 +21,8 @@ def test_homework():
 
     try:
         subprocess.run(
-            ["python3", "-m", "homework", "data/input", "data/output"],
-            check=True,
+            ["python3", "-m", "homework", "data/input/", "data/output/"],
+             check=True,
         )
     except subprocess.CalledProcessError as e:
         raise Exception(f"Error running the homework script: {e}")
